89bd7e3459d06a17d4acc6b39d5ba4eb2be3abec
Pavan Pinisetti <pavan.pinisetti@winfosolutions.com>
2025-09-29 11:02:17 +0530

    Ignore local scripts

--- app_export.sh
+++
@@ -1,7 +0,0 @@
-ACCEPT PROJECT_NAME CHAR PROMPT 'Enter Project Name: '
-ACCEPT APP_ID CHAR PROMPT 'Enter Application ID: '
-
-
-project init -name &PROJECT_NAME -schemas WKSP_EBS2CLOUD_MIGRATION
-
-project export -o apex.&APP_ID
